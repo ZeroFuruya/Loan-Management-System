93dff5de4818625e920c0ca9d700847665349568
Augustus Winchester <99241183+ZeroFuruya@users.noreply.github.com>
2023-01-18 09:53:11 +0800

    loan and status

--- src/main/java/e2p/icotp/model/Loan.java
+++ src/main/java/e2p/icotp/model/Loan.java
@@ -6,6 +6,11 @@ import e2p.icotp.model.Enums.Status;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Loan {
     private IntegerProperty loan_id;
@@ -19,11 +24,45 @@ public class Loan {
     private DoubleProperty due;
     private DoubleProperty paid;
     private DoubleProperty balance;
-    private Status status;
+    private StringProperty status;
+
+    public Loan(int loan_id, int loaner_id, LocalDate release_date, int term, LocalDate maturity_date, double principal,
+            double interest, double penalty, double due, double paid, double balance, String status) {
+
+        this.loan_id = new SimpleIntegerProperty(loan_id);
+        this.loaner_id = new SimpleIntegerProperty(loaner_id);
+        this.release_date = new SimpleObjectProperty<>(release_date);
+        this.term = new SimpleIntegerProperty(term);
+        this.maturity_date = new SimpleObjectProperty<>(maturity_date);
+        this.principal = new SimpleDoubleProperty(principal);
+        this.interest = new SimpleDoubleProperty(interest);
+        this.penalty = new SimpleDoubleProperty(penalty);
+        this.due = new SimpleDoubleProperty(due);
+        this.paid = new SimpleDoubleProperty(paid);
+        this.balance = new SimpleDoubleProperty(balance);
+        this.status = new SimpleStringProperty(status);
+    }
+
+    public Loan() {
+        this(0, 0, LocalDate.now(), 0, LocalDate.now(), 0, 0, 0, 0, 0, 0, Status.application.get());
+    }
 
     // SETTERS
 
+    public void setStatus(String val) {
+        this.status.set(val);
+        ;
+    }
+
     // GETTERS
 
+    public String getStatus() {
+        return this.status.get();
+    }
+
     // PROPERTY GETTERS
+
+    public StringProperty getStatusProperty() {
+        return this.status;
+    }
 }
