7a373b86bd7eea5bc1c87e5ff54e824068e8ed2c
Aloe Gel <119906246+annimalizz@users.noreply.github.com>
2023-01-23 14:58:50 +0800

    himoan na nako og mga FX id

--- src/main/java/e2p/icotp/layout/modal/LoanPlanController.java
+++ src/main/java/e2p/icotp/layout/modal/LoanPlanController.java
@@ -1,5 +1,29 @@
 package e2p.icotp.layout.modal;
 
+import com.itextpdf.text.pdf.TextField;
+
+import e2p.icotp.model.Enums.Status;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
 public class LoanPlanController {
-    
+    @FXML
+    private TextField installment;
+
+    @FXML
+    private ComboBox<Status> loanStatus;
+
+    @FXML
+    private TextField interest;
+
+    @FXML
+    private TextField repayment;
+
+    @FXML TextField annuity;
+
+    @FXML
+    private Button save;
+    @FXML
+    private Button cancel;
 }
--- src/main/java/e2p/icotp/layout/modal/PaymentController.java
+++ src/main/java/e2p/icotp/layout/modal/PaymentController.java
@@ -1,5 +1,28 @@
 package e2p.icotp.layout.modal;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class PaymentController {
-    
+    @FXML
+    private TextField lastname;
+
+    @FXML
+    private TextField loaner_id;
+
+    @FXML
+    private TextField loan_id;
+
+    @FXML
+    private DatePicker payment_date;
+
+    @FXML
+    private TextField payment_amount;
+
+    @FXML
+    private Button save;
+    @FXML
+    private Button cancel;
 }
