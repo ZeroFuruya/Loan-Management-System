11f9ff192494a266749ab6fd7b73d4c62d058992
Aloe Gel <119906246+annimalizz@users.noreply.github.com>
2023-01-25 16:35:28 +0800

    font ICon and tooltips

--- src/main/java/e2p/icotp/layout/modal/PaymentController.java
+++ src/main/java/e2p/icotp/layout/modal/PaymentController.java
@@ -1,10 +1,13 @@
 package e2p.icotp.layout.modal;
 
+import org.kordamp.ikonli.javafx.FontIcon;
+
 import e2p.icotp.App;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 
 public class PaymentController {
     @FXML
@@ -26,6 +29,31 @@ public class PaymentController {
     private Button save;
     @FXML
     private Button cancel;
+
+    // Icon
+    @FXML
+    private FontIcon lastname_icon;
+    @FXML
+    private FontIcon loanerID_icon;
+    @FXML
+    private FontIcon loanId_icon;
+    @FXML
+    private FontIcon paymentDate_icon;
+    @FXML
+    private FontIcon paymentAmount_icon;
+
+    // TOOLTIPS
+    @FXML
+    private Tooltip lastnameTT;
+    @FXML
+    private Tooltip loanerIdTT;
+    @FXML
+    private Tooltip loanIdTT;
+    @FXML
+    private Tooltip paymentDateTT;
+    @FXML
+    private Tooltip paymentAmountTT;
+
 
     private App app;
 
