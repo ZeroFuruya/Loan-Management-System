959c93b62a56be0f24ee18d1e3e4fec7e3f3a85f
Aloe Gel <119906246+annimalizz@users.noreply.github.com>
2023-01-26 16:44:13 +0800

    Done

--- src/main/java/e2p/icotp/layout/modal/LoanPlanController.java
+++ src/main/java/e2p/icotp/layout/modal/LoanPlanController.java
@@ -1,7 +1,6 @@
 package e2p.icotp.layout.modal;
 
 
-import org.kordamp.ikonli.javafx.FontIcon;
 
 import e2p.icotp.App;
 import e2p.icotp.model.Enums.Status;
@@ -9,6 +8,7 @@ import e2p.icotp.service.loader.ModalLoader;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 
@@ -36,13 +36,13 @@ public class LoanPlanController {
     // ICONS
 
     @FXML
-    private FontIcon installment_icon;
+    private Label installment_icon;
     @FXML
-    private FontIcon interest_icon;
+    private Label interest_icon;
     @FXML
-    private FontIcon repayment_icon;
+    private Label repayment_icon;
     @FXML
-    private FontIcon monthlyPenalty_icon;
+    private Label monthlyPenalty_icon;
 
     // Tooltips
     @FXML
@@ -68,5 +68,13 @@ public class LoanPlanController {
 
     public void load(App app){
         this.app = app;
+        load_bindings();
+    }
+
+    private void load_bindings(){
+        installment_icon.visibleProperty().bind(installment.textProperty().isEmpty());
+        interest_icon.visibleProperty().bind(interest.textProperty().isEmpty());
+        repayment_icon.visibleProperty().bind(repayment.textProperty().isEmpty());
+        monthlyPenalty_icon.visibleProperty().bind(monthlyPenalty.textProperty().isEmpty());
     }
 }
