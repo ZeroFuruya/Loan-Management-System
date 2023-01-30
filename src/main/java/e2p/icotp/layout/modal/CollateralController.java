2368665b680f1c705a69a2da6e87beb857734795
Aloe Gel <119906246+annimalizz@users.noreply.github.com>
2023-01-30 10:39:22 +0800

    done collateral

---
+++ src/main/java/e2p/icotp/layout/modal/CollateralController.java
@@ -0,0 +1,54 @@
+package e2p.icotp.layout.modal;
+
+import e2p.icotp.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+
+public class CollateralController {
+    @FXML
+    private TextField loanerId;
+    @FXML
+    private TextField loanId;
+    @FXML
+    private TextField collateral;
+
+    @FXML
+    private Button save;
+    @FXML
+    private Button cancel;
+
+    // Label
+    @FXML
+    private Label loanerIdLabel;
+    @FXML
+    private Label loanIdLabel;
+    @FXML
+    private Label collateralLabel;
+
+    // Tooltip
+    @FXML
+    private Tooltip loanerIdTT;
+    @FXML
+    private Tooltip loanIdTT;
+    @FXML
+    private Tooltip collateralTT;
+
+    private App app;
+
+    public void load(App app) {
+        this.app = app;
+        init_bindings();
+    }
+
+    private void init_bindings() {
+        loanerIdLabel.visibleProperty().bind(loanerId.textProperty().isEmpty());
+        loanIdLabel.visibleProperty().bind(loanId.textProperty().isEmpty());
+        collateralLabel.visibleProperty().bind(collateral.textProperty().isEmpty());
+
+        save.disableProperty().bind(loanerIdLabel.visibleProperty().or(loanIdLabel.visibleProperty())
+                .or(collateralLabel.visibleProperty()));
+    }
+}
